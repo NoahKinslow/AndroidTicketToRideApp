8e2cbc150458152fd8868696a6241e6fe652fe2b
Noah Kinslow <nkinslow@harding.edu>
2017-11-16 14:14:33 -0600

    Working on AuthenticationActivity

--- app/src/main/java/tewesday/androidtickettorideapp/AuthenticationActivity.java
+++ app/src/main/java/tewesday/androidtickettorideapp/AuthenticationActivity.java
@@ -1,11 +1,15 @@
 package tewesday.androidtickettorideapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +23,10 @@ public class AuthenticationActivity extends AppCompatActivity
 {
     private String TAG = AuthenticationActivity.class.getSimpleName();
     private FirebaseAuth mAuth;
+    private String mUsernameText;
+    private String mPasswordText;
+    private String mSignInText;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -33,7 +41,28 @@ public class AuthenticationActivity extends AppCompatActivity
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
+        if (currentUser != null)
+        {
+            //updateUI(currentUser);
+        }
+        else
+        {
+            // Ask user to either sign in or sign up
+            askUserForSignIn();
+            if (mSignInText == "Sign in")
+            {
+                // sign in
+                signIntoAccount(mUsernameText, mPasswordText);
+            }
+            else
+            {
+                // sign up
+                acquireUsername();
+                acquirePassword();
+                createAccount(mUsernameText, mPasswordText);
+            }
+
+        }
     }
 
     @Override
@@ -86,6 +115,114 @@ public class AuthenticationActivity extends AppCompatActivity
                         // ...
                     }
                 });
+    }
+
+    public void signIntoAccount(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(AuthenticationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void acquireUsername()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getApplicationContext());
+        builder.setTitle("Username");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mUsernameText = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void acquirePassword()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getApplicationContext());
+        builder.setTitle("Password");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mPasswordText = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void askUserForSignIn()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getApplicationContext());
+        builder.setTitle("Sign in or Sign Up");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mSignInText = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
 }
